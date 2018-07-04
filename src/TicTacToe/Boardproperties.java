694c6e16fad1dd8cc4c1a90842467062eadcbbd7
jenisonleo <jenisongracious@gmail.com>
2018-07-04 12:51:56 +0530

    addon

--- src/TicTacToe/Boardproperties.java
+++ src/TicTacToe/Boardproperties.java
@@ -2,7 +2,7 @@ package TicTacToe;
 
 public class Boardproperties {
     public static int ROWS = 3;
-    public static int COLS = 3;
+    public static int COLS = 4;
     public static int MOVEROW;
     public static int MOVECOL;
     public static int[][] board = new int[ROWS][COLS];
